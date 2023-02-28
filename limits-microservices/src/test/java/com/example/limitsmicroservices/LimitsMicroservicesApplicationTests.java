c202e77308dbc732ec7b9e9bdb3f843b395f481e
szejnaArtur <50076342+szejnaArtur@users.noreply.github.com>
2023-02-28 21:18:26 +0100

    First commit.

---
+++ limits-microservices/src/test/java/com/example/limitsmicroservices/LimitsMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.limitsmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
